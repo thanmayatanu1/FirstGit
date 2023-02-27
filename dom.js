c7a9afa34398dd25c30ea005db75e424cb3dbe8f
thanmayatanu1 <thanmayatanu1@gmail.com>
2023-02-27 12:13:52 +0530

    adding dom file

---
+++ dom.js
@@ -0,0 +1,8 @@
+console.log(document.title);
+console.log(document.URL);
+console.log(document.doctype);
+console.log(document.head);
+console.log(document.body);
+var headerTitle = document.getElementById('header-title');
+console.log(headerTitle);
+headerTitle.innerHTML = '<h3 style="color:green" "background:black";>Hello</h3>';
